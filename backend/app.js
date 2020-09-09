b0613efddeb733c0840824e54b3898148db1d634
francois <contact.fpweb@gmail.com>
2020-09-09 21:17:17 +0900

    install body parser + test signup post request

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+	);
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	next();
+});
+
+app.use(bodyParser.json());
+
+app.post("/api/auth/signup", (req, res, next) => {
+	console.log(req.body);
+});
 
 app.use((req, res) => {
 	res.json({ message: "Reçue !" });
